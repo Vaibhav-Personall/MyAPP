ff4abcdc77d5e54413f56579eb5317a319af427c
vainaik <vaibhav.a.naik@capgemini.com>
2012-06-08 11:17:29 +0200

    AFTER R1

--- src/Tester.java
+++ src/Tester.java
@@ -4,6 +4,7 @@ public class Tester {
 	public static void main(String[] args) {
         System.out.println("Manish");
 		System.out.println("Tester");
-		System.out.println("after merging");        
+		System.out.println("after merging");      
+		System.out.println("AFTER RELEASE 1");      
 	}
 }
